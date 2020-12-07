fe6599f6eed902f1fcdcc9c42ec631f86ce0f38c
keesha <kharg14@gmail.com>
2020-12-07 15:19:41 -0500

    create model

---
+++ db/migrate/20201207201211_create_movies.rb
@@ -0,0 +1,14 @@
+class CreateMovies < ActiveRecord::Migration[6.0]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.text :description
+      t.string :director
+      t.string :movie_length
+      t.integer :catergory_id
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
