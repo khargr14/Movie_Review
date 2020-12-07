ae732f9c8b886dbb0a557576593ded5cf706c163
keesha <kharg14@gmail.com>
2020-12-07 17:34:33 -0500

    create new forms

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,27 @@
 class MoviesController < ApplicationController
 
     def index
+        @movies = Movie.all
     end
+
+    def new
+        @movie = Movie.new 
+    end
+
+    def create
+        @movie = Movie.new(movie_params)
+
+        if @movie.save #meaning create
+            redirect_to root_path
+        else
+            render 'new'       
+        end
+    end
+
+
+    private
+        def movie_params
+            params.require(:book).permit(:title, :description, :director, :movie_length)
+        end
+
 end
