bf761cbde10b0e39e9167656a9c2def626458008
keesha <kharg14@gmail.com>
2020-12-14 20:11:08 -0500

    setting up models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
    has_many :movies
+   has_many :reviews
+   has_many :movies, through: :reviews 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
