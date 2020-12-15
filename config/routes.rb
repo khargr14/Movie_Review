c8158bab1d5e9b6e046b1d887eb5effc31a156b9
keesha <kharg14@gmail.com>
2020-12-14 21:18:45 -0500

    created review controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :movies
+  
+  resources :movies do
+    resources :reviews
+  end
+
+
   root 'movies#index'
 end
