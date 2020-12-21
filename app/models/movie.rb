f57891e4e7f97ca50b1093af68c93406076a02f0
keesha <kharg14@gmail.com>
2020-12-20 23:37:24 -0500

    validate some of the forms

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,6 +6,10 @@ class Movie < ApplicationRecord
     scope :movies_desc_order, -> { all.order("created_at DESC") }
     has_attached_file :movie_img, styles: { movie_index: "250x350>", movie_show: "325x475>" }, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :movie_img, content_type: /\Aimage\/.*\z/
+    validates :title, presence: true
+    validates :description, presence: true
+    
+
 
     def ratings
         self.reviews.collect(&:rating)
